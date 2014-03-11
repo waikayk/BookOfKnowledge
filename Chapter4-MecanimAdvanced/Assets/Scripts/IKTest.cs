816a4a860da51f4cf6f75b1441dbe904628ca1bd
Wai Kay Kong <Kong_WaiKay@bah.com>
2014-03-11 15:39:40 -0400

    Mecanim stuff and Gyromote stuff

---
+++ Chapter4-MecanimAdvanced/Assets/Scripts/IKTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKTest : MonoBehaviour {
+	public Animator anim;
+	public Transform ikTarget;
+	
+	bool isIKTracking;
+	
+	void Start(){
+		
+	}
+	
+	void Update(){
+		if(Input.GetKeyDown("t")){
+			if(isIKTracking){
+				StartCoroutine(IKTarget());
+			}
+			else{
+				isIKTracking = false;
+			}
+		}
+	}
+	
+	IEnumerator IKTarget(){
+		isIKTracking = true;
+		while(isIKTracking){
+			
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
