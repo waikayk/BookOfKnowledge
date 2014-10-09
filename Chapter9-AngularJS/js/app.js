5e17e65ef50dbd3e447b6bec73b0dd6eb36bf317
Wai Kay Kong <Kong_WaiKay@bah.com>
2014-10-09 17:51:11 -0400

    Actually CSS formatting a Calculator.

--- Chapter9-AngularJS/js/app.js
+++ Chapter9-AngularJS/js/app.js
@@ -1,10 +1,20 @@
 var app = angular.module('calcApp',[]);
 
 app.controller('CalcController', function(){
-	this.Answer = "";
+	this.Answer = "0";
 	this.Question = "";
 	
 	this.Parrot = function(sentence){
+		
 		this.Answer = sentence;
+	};
+});
+
+app.directive("calcButtons", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/calc-buttons.html",
+		controller: "CalcController",
+		controllerAs: "calc"
 	};
 });
