fd3fefa25aee7626d4cf6f09c2c212b8c64a14c8
waikayk <azkonamage@gmail.com>
2014-10-10 16:11:37 -0400

    Angular claculator works

--- Chapter9-AngularJS/js/app.js
+++ Chapter9-AngularJS/js/app.js
@@ -1,12 +1,53 @@
 var app = angular.module('calcApp',[]);
 
 app.controller('CalcController', function(){
-	this.Answer = "0";
-	this.Question = "";
+	this.currentNumber = "";
+	this.currentTotal = "0";
+	this.displayThis = "0";
+	this.mathAction = "";
 	
-	this.Parrot = function(sentence){
+	this.CreateNumber = function(digit){
+		this.currentNumber += digit;
+		this.displayThis = this.currentNumber;
+	};
+	
+	this.SetMathAction = function(symbol){
+		if(this.mathAction !== ""){
+			this.ProcessEquals();
+		}
+		else{
+			if(this.currentNumber !== ""){
+				this.currentTotal = this.currentNumber;
+			}
+		}
+		this.currentNumber = "";
+		this.mathAction = symbol;
+	};
+	
+	this.ProcessEquals = function(){
+		if(this.mathAction === "" || this.currentNumber === "") return;
 		
-		this.Answer = sentence;
+		if(this.mathAction === '+'){
+			this.currentTotal = parseFloat(this.currentTotal) + parseFloat(this.currentNumber);
+		}
+		else if(this.mathAction === '-'){
+			this.currentTotal = parseFloat(this.currentTotal) - parseFloat(this.currentNumber);
+		}
+		else if(this.mathAction === '*'){
+			this.currentTotal = parseFloat(this.currentTotal) * parseFloat(this.currentNumber);
+		}
+		else if(this.mathAction === '/'){
+			this.currentTotal = parseFloat(this.currentTotal) / parseFloat(this.currentNumber);
+		}
+
+		this.displayThis = this.currentTotal;
+	};
+	
+	this.Clear = function(){
+		this.currentTotal = "0";
+		this.currentNumber = "";
+		this.displayThis = "0";
+		this.mathAction = "";
 	};
 });
 
