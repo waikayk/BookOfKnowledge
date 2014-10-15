8cbd3c854ef9288e4f2b856de9c531202820a05d
Wai Kay Kong <Kong_WaiKay@bah.com>
2014-10-15 16:02:27 -0400

    Added bootstrap project and angular-directoried the hell out of it to make the html way more readable.

---
+++ Chapter10-Bootstrap/js/directives.js
@@ -0,0 +1,111 @@
+app.directive("bootstrapFixedNavbar", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-fixed-navbar.html"
+	};
+});
+
+app.directive("bootstrapMainJumbotron", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-main-jumbotron.html"
+	};
+});
+
+app.directive("bootstrapButtons", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-buttons.html"
+	};
+});
+
+app.directive("bootstrapTables", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-tables.html"
+	};
+});
+
+app.directive("bootstrapThumbnails", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-thumbnails.html"
+	};
+});
+
+app.directive("bootstrapLabels", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-labels.html"
+	};
+});
+
+app.directive("bootstrapBadges", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-badges.html"
+	};
+});
+
+app.directive("bootstrapDropdownMenus", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-dropdown-menus.html"
+	};
+});
+
+app.directive("bootstrapNavs", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-Navs.html"
+	};
+});
+
+app.directive("bootstrapNavbars", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-navbars.html"
+	};
+});
+
+app.directive("bootstrapAlerts", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-alerts.html"
+	};
+});
+
+app.directive("bootstrapProgressBars", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-progress-bars.html"
+	};
+});
+
+app.directive("bootstrapListGroups", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-list-groups.html"
+	};
+});
+
+app.directive("bootstrapPanels", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-panels.html"
+	};
+});
+
+app.directive("bootstrapWells", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-wells.html"
+	};
+});
+
+app.directive("bootstrapCarousel", function(){
+	return{
+		restrict: 'E',
+		templateUrl: "html/bootstrap-carousel.html"
+	};
+});
