be19f56dc1ede840034b034c628a29857355f3bf
Wai Kay Kong <Kong_WaiKay@bah.com>
2014-10-15 18:29:59 -0400

    making some of the more redundant html into looping ANGULAR js

--- Chapter10-Bootstrap/js/app.js
+++ Chapter10-Bootstrap/js/app.js
@@ -1,2 +1,5 @@
 var app = angular.module('app',[]);
 
+app.controller('AppController', function(){
+	this.people = people;
+});
---
+++ Chapter10-Bootstrap/js/data.js
@@ -0,0 +1,22 @@
+var people = [
+	{
+		number: '1',
+		nameFirst: 'Chris',
+		nameLast: 'Cker',
+		nameUser: '@ChTu'
+	},
+	{
+		number: '2',
+		nameFirst: 'Fudge',
+		nameLast: 'Tucker',
+		nameUser: '@ChTMe'
+	},
+	{
+		number: '3',
+		nameFirst: 'Cup',
+		nameLast: 'Tucker',
+		nameUser: '@Chlllu'
+	}
+];
+
+
