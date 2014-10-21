02e1172f599e4f5e0900c7d3c9fb2dba812c31c4
Wai Kay Kong <Kong_WaiKay@bah.com>
2014-10-21 18:14:53 -0400

    Finally got a FBx to JSON converter working.

--- Chapter11-ThreeJS/js/game.js
+++ Chapter11-ThreeJS/js/game.js
@@ -1,111 +1,30 @@
-var container;
-var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
- 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
- 
 var modelPath = "mesh/sephir_final.obj";
 var texturePath = "texture/sephir_tex.png";
- 
+
+var WIDTH = 640;
+var	HEIGHT = 480;
+
+var container;
+
+var camera, scene, renderer;
+
+var ambient, directionalLight;
+
+var mouseX = 0, mouseY = 0;
+
+var loadSphere = false;
+
 init();
 animate();
- 
-/*** Initialize ***/
-function init() {
-  // This <div> will host the canvas for our scene.
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
- 
-  // You can adjust the cameras distance and set the FOV to something
-  // different than 45‹. The last two values set the clippling plane.
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-  camera.position.z = 100;
- 
-  // These variables set the camera behaviour and sensitivity.
-  controls = new THREE.TrackballControls( camera );
-  controls.rotateSpeed = 5.0;
-  controls.zoomSpeed = 5;
-  controls.panSpeed = 2;
-  controls.noZoom = false;
-  controls.noPan = false;
-  controls.staticMoving = true;
-  controls.dynamicDampingFactor = 0.3;
- 
-  // This is the scene we will add all objects to.
-  scene = new THREE.Scene();
- 
-  // You can set the color of the ambient light to any value.
-  // I have chose a completely white light because I want to paint
-  // all the shading into my texture. You propably want something darker.
-	var ambient = new THREE.AmbientLight( 666666 );
-	scene.add( ambient );
- 
-  // Uncomment these lines to create a simple directional light.
-	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-	directionalLight.position.set( 0, 0, 1 ).normalize();
-	scene.add( directionalLight );
-  
-  // create a point light
-	/* var pointLight =
-	new THREE.PointLight(0xFFFFFF);
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130; */
-	// add to the scene
-	//scene.add(pointLight);
- 
-  /*** Texture Loading ***/
-  var manager = new THREE.LoadingManager();
-  manager.onProgress = function ( item, loaded, total ) {
-    console.log( item, loaded, total );
-  };
-  var texture = new THREE.Texture();
-  var loader = new THREE.ImageLoader( manager );
- 
-  // You can set the texture properties in this function. 
-  // The string has to be the path to your texture file.
-  loader.load( texturePath, function ( image ) {
-    texture.image = image;
-    texture.needsUpdate = true;
-    // I wanted a nearest neighbour filtering for my low-poly character,
-    // so that every pixel is crips and sharp. You can delete this lines
-    // if have a larger texture and want a smooth linear filter.
-    texture.magFilter = THREE.NearestFilter;
-    texture.minFilter = THREE.NearestMipMapLinearFilter;
-  } );
- 
-  /*** OBJ Loading ***/
-  var loader = new THREE.OBJLoader( manager );
- 
-  // As soon as the OBJ has been loaded this function looks for a mesh
-  // inside the data and applies the texture to it.
-  loader.load( modelPath, function ( event ) {
-    var object = event;
-    object.traverse( function ( child ) {
-      if ( child instanceof THREE.Mesh ) {
-        child.material.map = texture;
-      }
-    } );
- 
-    // My initial model was too small, so I scaled it upwards.
-    object.scale = new THREE.Vector3( 25, 25, 25 );
- 
-    // You can change the position of the object, so that it is not
-    // centered in the view and leaves some space for overlay text.
-    object.position.y -= 2.5;
-    scene.add( object );
-  });
- 
-  // We set the renderer to the size of the window and
-  // append a canvas to our HTML page.
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  container.appendChild( renderer.domElement );
+
+function init(){
+	scene = new THREE.Scene();
+	SetCameraSettings();
+	SetLights();
+	Load();
+	RenderScene();
 }
- 
-/*** The Loop ***/
+
 function animate() {
   // This function calls itself on every frame. You can for example change
   // the objects rotation on every call to create a turntable animation.
@@ -116,4 +35,101 @@ function animate() {
   controls.update();
   camera.lookAt(scene.position);
   renderer.render(scene, camera);
+}
+
+function SetCameraSettings(){
+	camera = new THREE.PerspectiveCamera( 45, WIDTH / HEIGHT, 1, 2000 );
+	camera.position.z = 40;
+	camera.position.y = 0;
+	camera.position.x = 35;
+	
+	controls = new THREE.TrackballControls( camera );
+	controls.rotateSpeed = 5.0;
+	controls.zoomSpeed = 5;
+	controls.panSpeed = 2;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	
+	scene.add(camera);
+}
+
+function SetLights(){
+	ambient = new THREE.AmbientLight(666666);
+	scene.add(ambient);
+ 
+	directionalLight = new THREE.DirectionalLight(0xffeedd);
+	directionalLight.position.set( 0, 0, 1 ).normalize();
+	scene.add(directionalLight);
+}
+
+function Load(){
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+		console.log( item, loaded, total );
+	};
+	
+	//Texture
+	var texture = new THREE.Texture();
+	var loader = new THREE.ImageLoader( manager );
+	loader.load( texturePath, function ( image ) {
+		texture.image = image;
+		texture.needsUpdate = true;
+	} );
+
+	//Mesh OBJ
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( modelPath, function ( event ) {
+		var object = event;
+		object.traverse(function(child){
+		  if (child instanceof THREE.Mesh){
+			child.material.map = texture;
+		  }
+		});
+		object.position.y -= 10;
+		scene.add(object);
+	});
+}
+
+function RenderScene(){
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(0x2F4F4F, 1);
+	renderer.setSize(WIDTH, HEIGHT);
+	container = document.getElementById('container');
+	container.appendChild(renderer.domElement);
+	renderer.render(scene, camera);
+}
+
+function MakeSphere(){
+	// create the sphere's material
+	var sphereMaterial = new THREE.MeshLambertMaterial(
+	{
+	    color: 0xCC0000
+	});
+
+	// set up the sphere vars
+	var radius = 50, segments = 16, rings = 16;
+
+	// create a new mesh with sphere geometry -
+	// we will cover the sphereMaterial next!
+	var sphere = new THREE.Mesh(
+	   new THREE.SphereGeometry(radius, segments, rings),
+	   sphereMaterial
+	);
+
+	sphere.scale.set(0.1, 0.1, 0.1);
+	
+	// add the sphere to the scene
+	scene.add(sphere);
+}
+
+function ClearScene(){
+	var obj, i;
+	for ( i = scene.children.length - 1; i >= 0 ; i -- ) {
+		obj = scene.children[ i ];
+		if ( obj.name !== ambient && obj !== directionalLight && obj !== camera) {
+			scene.remove(obj);
+		}
+	}
 }
