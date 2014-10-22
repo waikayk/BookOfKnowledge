7705d21dcc91fff66ee92f0dead221306f23541f
waikayk <azkonamage@gmail.com>
2014-10-22 17:46:33 -0400

    Made it easy to add new models to the gallery.

---
+++ Chapter11-ThreeJS/js/modelpaths.js
@@ -0,0 +1,12 @@
+var assetPaths = [
+	{
+		name: "Dragon",
+		model: "mesh/sephir.obj",
+		texture: "texture/sephir_tex.png",
+	},
+	{
+		name: "Scythe",
+		model: "mesh/scythe.obj",
+		texture: "texture/scythe_tex.jpg",
+	}
+];
